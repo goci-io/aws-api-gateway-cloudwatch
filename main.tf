f2972fc7f1ae32f7f523368731456aab9ab4973d
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-27 18:38:37 +0100

    resolve type mismatch for list

--- main.tf
+++ main.tf
@@ -72,8 +72,8 @@ data "aws_iam_policy_document" "custom" {
 
     content {
       effect    = "Allow"
-      actions   = [statement.value.actions]
-      resources = [statement.value.resources]
+      actions   = statement.value.actions
+      resources = statement.value.resources
     }
   }
 }
